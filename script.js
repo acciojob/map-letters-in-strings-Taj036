0da5532acb163a06b8c2688eee83cfc3595e5bf1
Taj036 <121137573+Taj036@users.noreply.github.com>
2023-12-14 23:10:14 +0000

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function letter(word) {
+	let mapletter ={}
+	for(let i = 0 ; i < word.length ; i++){
+		let letter = word[i];
+		if(mapletter.hasOwnProperty(letter)){
+			mapletter[letter].push(i);
+		}
+		else{
+			mapletter[letter] = [i];
+		}
+	}
+	return mapletter ;
+}
